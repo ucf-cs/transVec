70ca4dbcc7312c1aae3c76d79fba6cfc2b6ee31c
sorazy <salnaizy@knights.ucf.edu>
2019-08-14 11:20:29 -0400

    clean up

--- graphs/GenerateGraph.py
+++ graphs/GenerateGraph.py
@@ -8,16 +8,16 @@ import sys
 col = ['DS', 'TESTCASE', 'SGMT_SIZE', 'NUM_TXN', 'TXN_SIZE', 'THRD_CNT', 'TIME', 'ABORTS', 'SYSTEM', 'THRUPUT', 'RELATIVE']
 df  = pd.read_csv('all_systems_final.txt', sep='\t', names=col, skiprows=1)
 
-# The plot settings (There are a few settings you can mess around with. They're all on Google)
+# The plot settings (There are a few settings you can mess around with. They're all on Google. I'll be using the defaults)
 sns.set()
 
-# Filter the data
+# Filter the data (Add or remove as many parameters as you want)
 fil = df.loc[(df['SYSTEM']=='ARMS') & (df['TESTCASE']==1) & (df['TXN_SIZE']==5)]
 
 # There's normally a grey-ish background for some reason. This makes it completely white
 sns.set_style(style='white')
 
-# Plot the data
+# Plot the data. The seaborn.lineplot documentation (Google) gives descriptions of each of these paramters (and more)
 g = sns.lineplot(data=fil, x='THRD_CNT', y='THRUPUT', hue='DS', style='SYSTEM', err_style=None, markers=True, dashes=False)
 
 # Label the axes
