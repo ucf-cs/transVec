077892c619e0848cbbd395e918011e3552ec64a8
Kenneth Lamar <kenneth@knights.ucf.edu>
2019-08-20 16:00:00 -0400

    Sanitizer now removes failed tests too.

--- graphs/sanitize.sh
+++ graphs/sanitize.sh
@@ -1,6 +1,9 @@
 # Delete all the lines with the word 'Pool'
 sed -i 's/Pool.*//' $1
 
+# Delete lines corresponding to failed tests
+sed -i '/^-1$/d' $1
+
 # Get rid of all the empty lines
 sed -i '/^$/d' $1
 
